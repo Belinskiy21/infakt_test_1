29ddb1e670663d2da3a04bb3acbbedcb0240ceb5
Oleg <belinskiy21@gmail.com>
2018-05-17 20:32:26 +0300

    cipher is done

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class CeasarCipher
 
+  def initialize(string, shift = 13)
+    @string = string
+    @shift = shift
+  end
+
+  def perform
+    alphabet  = Array('a'..'z')
+    non_caps  = Hash[alphabet.zip(alphabet.rotate(@shift))]
+
+    alphabet = Array('A'..'Z')
+    caps     = Hash[alphabet.zip(alphabet.rotate(@shift))]
+
+    encrypter = non_caps.merge(caps)
+
+    @string.chars.map { |c| encrypter.fetch(c, c) }.join
+  end
 end
